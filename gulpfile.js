aee5b135c361756f3015fb206532210ca840d86c
Frode Sundby <frodesun@gmail.com>
2015-11-18 22:07:41 +0100

    Plain Bootstrap template ready

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,36 @@ var minifyCSS = require('gulp-minify-css');
 var size = require('gulp-size')
 
 var paths = {
-	source: ['./src/index.html', './src/build.html'],
-	dest: './build',
-  css: './src/css/**/*.css',
+	htmlSource: ['./src/index.html', './src/build.html'],
+  cssSource: './src/css/**/*.css',
   extCss: './src/ext/css/**/*.css',
-  buildDir: './build',
+  jsSource: './src/js/**/*.js',
+  htmlBuild: './build',
+  jsBuild: './build/js',
   cssBuild: './build/css'
 }
 
 gulp.task('watch-folder', function() {  
-  gulp.watch(paths.source, ['copy-folder']);
+  gulp.watch(paths.htmlSource, ['copy-html']);
 });
 
-gulp.task('copy-folder', function() {  
-  gulp.src(paths.source)
-    .pipe(gulp.dest(paths.dest));
+gulp.task('copy-html', function() {  
+  gulp.src(paths.htmlSource)
+    .pipe(gulp.dest(paths.htmlBuild));
+});
+
+gulp.task('copy-js', function() {  
+  gulp.src(paths.jsSource)
+    .pipe(gulp.dest(paths.jsBuild));
 });
 
 gulp.task('bundle-css', function () {
-    return gulp.src([paths.extCss, paths.css])
+    return gulp.src([paths.extCss, paths.cssSource])
         .pipe(concat('bundle.css'))
         .pipe(minifyCSS())
         .pipe(size())
         .pipe(gulp.dest(paths.cssBuild));
 });
 
-gulp.task('default', ['bundle-css', 'watch-folder'], function() {
+gulp.task('default', ['copy-html', 'copy-js', 'bundle-css', 'watch-folder'], function() {
 })
